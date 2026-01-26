19395f70220c0d754df7f01f38d6e186d9bb7cfa
Sher <harshkarira2004@gmail.com>
2026-01-26 23:39:14 +0530

    added controller and request mapping annotations

--- src/main/java/com/codebase/time_traveller/api/TimeTravelController.java
+++ src/main/java/com/codebase/time_traveller/api/TimeTravelController.java
@@ -4,9 +4,12 @@ import com.codebase.time_traveller.api.dto.TimeTravelRequest;
 import com.codebase.time_traveller.api.dto.TimeTravelResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/timetravel")
 public class TimeTravelController {
     private final TimeTravelFacade facade;
 
